5f6a835eea01dc25d48d04f63f424a5def141ff2
Пользователь <stepanyukilja@gmail.com>
2020-09-28 10:59:57 +0300

    add form and last fixed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-// $(document).ready(function () {
+$(document).ready(function () {
+   let link = $('.form__footer');
+   let table = $('.table');
+   let arrow = $('.arrow');
+   let form = $('.form');
 
-// });
+   link.click(function () {
+      table.toggleClass('active');
+      arrow.toggleClass('active');
+      form.toggleClass('active');
+   });
+});
